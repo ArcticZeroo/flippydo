b041b3b87054fa6ee5ed08f3467927ad724583a9
ArcticZeroo <spencer@artix.cc>
2016-09-21 10:32:47 -0400

    Add styling, dynamic amount input.

--- binary.js
+++ binary.js
@@ -1,20 +1,12 @@
 var gen_count = 11;
 $(document).ready(function () {
-    //var labels = document.getElementsByClassName('label');
-    //for(var label of labels){
+    generate(gen_count);
+    setBinaryClick();
 
-    for(var i = gen_count; i >= 0; i--){
-        $('#label').append('<td>2<sup>' + i + '</sup></td>');
-        $('#val').append('<td>' + Math.pow(2, i) + '</td>');
-        $('#num').append('<td class="binary" id="' + i + '">0</td>');
-    }
-
-    $('.binary').click(function(){
-        var self = $(this);
-        var val = self.html();
-        if(val == '0') self.html('1');
-        else self.html('0');
-        $('#value').html(getTotal().toString());
+    $('#changecount').click(function(){
+        gen_count = $('#itemcount').val();
+        generate(gen_count);
+        setBinaryClick();
     });
 })
 
@@ -25,4 +17,30 @@ function getTotal(){
         if($('#'+i).html() == 1) total += Math.pow(2, i);
     }
     return total;
+}
+
+function generate(amount){
+    var labelElement  = $('#label');
+    var valElement    = $('#val');
+    var binaryElement = $('#num');
+
+    labelElement.empty();
+    valElement.empty();
+    binaryElement.empty();
+
+    for(var i = gen_count; i >= 0; i--){
+        labelElement.append('<td>2<sup>' + i + '</sup></td>');
+        valElement.append('<td>' + Math.pow(2, i) + '</td>');
+        binaryElement.append('<td class="binary" id="' + i + '">0</td>');
+    }
+}
+
+function setBinaryClick(){
+    $('.binary').click(function(){
+        var self = $(this);
+        var val = self.html();
+        if(val == '0') self.html('1');
+        else self.html('0');
+        $('#value').html(getTotal().toString());
+    });
 }
